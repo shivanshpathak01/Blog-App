b611ca87984d61c77da0b5bc9b9c92ab9f9ee573
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-02-15 15:25:27 +0530

    ..

--- MegaBlog/src/components/PostCard.jsx
+++ MegaBlog/src/components/PostCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import appwriteService from "../appwrite/config"
 import {Link} from 'react-router-dom'
-/* 14:22 */
+
 function PostCard({$id, title, featuredImage}) {
     
   return (
