2abede5af77724172d512141e9c6d97d644fe21b
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-02-18 23:55:25 +0530

    some changes as logo and copyright

--- MegaBlog/src/components/Logo.jsx
+++ MegaBlog/src/components/Logo.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div>
-      Logo
+      <img 
+        src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png"
+        alt="Blog Logo"
+        className="w-full"
+        style={{ width }}
+      />
     </div>
   )
 }
