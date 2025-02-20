451a59edd4242ac96be85eaeed967906be22880e
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-02-20 22:54:10 +0530

    ..

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -12,7 +12,6 @@ import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
-import Editor from "./Editor";
 
 export {
     Header,
@@ -29,5 +28,4 @@ export {
     AuthLayout, 
     Input,
     Select,
-    Editor
 }
